f1c0b2c333affc2013f4dd00075a234d6f528a7c
Marek Dwulit <marek.dwulit@agilebeat.com>
2020-06-04 02:42:41 +0000

    Working deployment inside lamnda

--- graphql.js
+++ graphql.js
@@ -3,7 +3,10 @@ const { ApolloServer } = require("apollo-server-lambda");
 
 const schema = require("./graphql/schema");
 
-const server = new ApolloServer(schema);
+const server = new ApolloServer({
+  schema,
+  graphiql: true
+});
 
 exports.graphqlHandler = server.createHandler({
   cors: {
