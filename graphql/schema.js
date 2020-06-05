643d51737bd363004faa6ce7994b686d5ee8d5e6
Marek Dwulit <marek.dwulit@agilebeat.com>
2020-06-05 04:39:42 +0000

    Local version is working

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,43 +1,62 @@
+'use strict';
 const graphql = require('graphql');
 const _ = require('lodash');
 const moment = require('moment');
 const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString, GraphQLInt } = graphql;
+const fs = require('fs');
 
-//dummy data
-var statesData = [
-    {
-        "cases": "1",
-        "county": "Butler",
-        "date": "2020-03-28",
-        "deaths": "0",
-        "fips": "21031",
-        "state": "Kentucky",
-        "state_county": "Kentucky_Butler"
-    },
-    {
-        "cases": "200",
-        "county": "Butler",
-        "date": "2020-04-12",
-        "deaths": "6",
-        "fips": "21031",
-        "state": "Kentucky",
-        "state_county": "Kentucky_Butler"
-    },
-    {
-        "cases": "200",
-        "county": "Butler",
-        "date": "2020-05-12",
-        "deaths": "6",
-        "fips": "21031",
-        "state": "Virginia",
-        "state_county": "Kentucky_Butler"
+const dataLoader = (function () {
+  var rec_list = [];
+
+  function loadDataFromFs(data_path) {
+    try {
+      const data = fs.readFileSync(data_path, 'UTF-8');
+
+      let lines = data.split(/\r?\n/);
+      let h_line = lines.shift();
+      let h = h_line.split(',');
+      var rec_list = [];
+
+      lines.forEach((line) => {
+        let l = line.split(',');
+        let rec_dict = {};
+        for (let i = 0; i < h.length; i++) {
+          if (h[i] == 'cases' || h[i] == 'deaths') {
+            rec_dict[h[i]] = parseInt(l[i]);
+          } else {
+            rec_dict[h[i]] = l[i];
+          }
+        }
+        rec_list.push(rec_dict);
+      });
+    } catch (err) {
+      console.error(err);
+      return [];
     }
-];
+    return rec_list;
+  }
+
+  function loadData(URI) {
+    return loadDataFromFs(URI);
+  };
+
+  return {
+    getData: function (URI) {
+      if (rec_list.length == 0) {
+        rec_list = loadData(URI);
+      }
+      return rec_list;
+    }
+  }
+})();
+
+const csvFilePath = './data/us-counties.csv';
+const statesData = dataLoader.getData(csvFilePath);
+console.log(statesData)
 
 const StateType = new GraphQLObjectType({
     name: 'State',
     fields: () => ({
-        state_county: { type: GraphQLString },
         state: { type: GraphQLString },
         county: { type: GraphQLString },
         date: { type: GraphQLString },
@@ -63,7 +82,6 @@ const RootQuery = new GraphQLObjectType({
                     let end = moment(args.end);
                     let isAfterStart = (start < odate);
                     let isBeofreOrEqualToEnd = (odate <= end);
-                    console.log(o.state, args.state, (o.state == args.state), isAfterStart, isBeofreOrEqualToEnd);
                     return ((o.state == args.state) && isAfterStart && isBeofreOrEqualToEnd)
                 });
                 return retset;
